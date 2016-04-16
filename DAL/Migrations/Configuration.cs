c162fae493626843e248d22c71a7990dcb1d3217
xKamiLx <xdreamer93@o2.pl>
2016-04-16 12:27:59 +0200

    Added Newses Controller, and default views.

--- DAL/Migrations/Configuration.cs
+++ DAL/Migrations/Configuration.cs
@@ -13,6 +13,7 @@ namespace DAL.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(Portal.Models.ApplicationDbContext context)
---
+++ Portal/ViewModels/NewsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL.Models;
+using Portal.Models;
+
+namespace Portal.ViewModels
+{
+    public class NewsViewModel
+    {
+        public NewsViewModel()
+        {
+        }
+        public NewsViewModel(News news)
+        {
+            Id = news.Id;
+            Title = news.Title;
+            Description = news.Description;
+            AddedDateTime = news.AddedDateTime;
+            ApplicationUser = news.ApplicationUser;
+        }
+
+        //TODO
+        //MaQ read about Attributes, and use them to this properties
+        //http://www.asp.net/mvc/overview/older-versions/getting-started-with-aspnet-mvc4/adding-validation-to-the-model
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime AddedDateTime { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
